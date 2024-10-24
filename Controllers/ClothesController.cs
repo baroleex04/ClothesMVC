7e636814ed277c33d82951a077b66cf69c1e8ffa
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-10-24 18:00:03 +0700

    Add project files.

---
+++ Controllers/ClothesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesMVC.Controllers
+{
+    public class ClothesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClothesMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
